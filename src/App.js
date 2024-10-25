2aa54d94d251b4daa534462414eda1e39fe8cba2
kayky <kaykyzioti@gmail.com>
2024-10-25 10:44:21 -0300

    veaevae

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './pages/Login'
+import Login from './pages/Login';
 
 // Componente de página não encontrada
 const NotFound = () => <h2>404 - Página não encontrada</h2>;
@@ -8,7 +8,7 @@ const NotFound = () => <h2>404 - Página não encontrada</h2>;
 function App() {
   return (
     <div>
-      <Router>
+      <Router basename="/site-marcar-aulas"> {/* Adicione o basename aqui */}
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="*" element={<NotFound />} />
